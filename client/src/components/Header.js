765c95355b12a6b2af29203f10424088ae1e259e
rajasanthosh <rajasanthoshm@gmail.com>
2024-02-16 11:10:36 +0530

    check the user logged-in before based on change the nav-bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../App.css';
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [userInfo,setUserInfo]=useState(null)
+  useEffect(()=>{
+    fetch('http://localhost:8000/profile',{
+    credentials: 'include',
+  }).then(response => {
+    response.json().then(userInfo => {
+      setUserInfo(userInfo);
+    });
+  });
+}, []);
   return (
     <header>
       <Link to="/" className="logo">
         MyBlog
       </Link>
+      
       <nav>
-        <Link to="/login">Login</Link>
-        <Link to="/registration">SignUp</Link>
+      {(userInfo && (
+        <>
+        <p>{userInfo.username}</p>
+          <Link to=''>Create a post</Link>
+          <Link to=''>Logout</Link>
+
+        </>
+      ))}
+      {!userInfo && (
+        <>
+          <Link to="/login">Login</Link>
+          <Link to="/registration">SignUp</Link>
+        </>
+      )}
+        
       </nav>
     </header>
   );
--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const User = require("./models/User");
 const bcrypt = require("bcryptjs");
 const app = express();
 const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 const salt = bcrypt.genSaltSync(10);
 const secret = "sdfghbn1324rtgq4rs";
 
 app.use(cors({credentials: true, origin:'http://localhost:3000'}));
 app.use(express.json());
+app.use(cookieParser())
 //
 //mongodb+srv://rajasanthoshm:US2yi78PQtHaERIh@cluster0.mweuvvr.mongodb.net/?retryWrites=true&w=majority
 
@@ -42,4 +44,14 @@ app.post("/login", async (req, res) => {
     })
   } else res.status(400).json("notcool");
 });
+
+app.get('/profile',async(req,res)=>{
+  const {token} = req.cookies;
+  jwt.verify(token,secret,{},(err,info)=>{
+    if(err) throw err;
+    res.json(info);
+    
+  })
+})
+
 app.listen(8000, () => console.log("server started listening"));
